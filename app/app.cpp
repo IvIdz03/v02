2e6a805dfe713487add024f520e862b0747a993a
IvIdz03 <ividzano@vsite.hr>
2025-05-20 18:52:13 +0200

    uredeni app.cpp app.h i main.cpp

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,44 @@
 #include "app.h"
 #include <algorithm>
+#include <format>
+#include <iostream>
 
 namespace vsite::oop::v2
 {
-/*
-* class member function implementations
-*/
+	std::string to_hex(int number) {
+		return std::format("{:X}", number);
+	}
 
+	double color::get_red() const {
+		return red;
+	}
+
+	double color::get_green() const {
+		return green;
+	}
+
+	double color::get_blue() const {
+		return blue;
+	}
+
+	void color::set_red(double value) {
+		red = std::clamp(value, 0.0, 1.0);
+	}
+
+	void color::set_green(double value) {
+		green = std::clamp(value, 0.0, 1.0);
+	}
+
+	void color::set_blue(double value) {
+		blue = std::clamp(value, 0.0, 1.0);
+	}
+
+	unsigned int color::get_color_ref() const {
+		return RGB(static_cast<int>(red * 255), static_cast<int>(green * 255), static_cast<int>(blue * 255));
+	}
+
+	double color::get_luminance() const {
+		return 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+	}
 
 } // namespace
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <format>
+#include <string>
 #include "app.h"
 
 int main()
 {
+	vsite::oop::v2::color c;
+
+	double r, g, b;
+	std::cout << "unesi crvenu, zelenu i plavu: ";
+	std::cin >> r >> g >> b;
+
+	c.set_red(r);
+	c.set_green(g);
+	c.set_blue(b);
+
+	std::cout << "Vrijednost boje u hex zapisu 0x" << vsite::oop::v2::to_hex(c.get_color_ref());
 }
